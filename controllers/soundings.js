ed6e48eefbe2b9860e1c7a20ee49550601100e93
ethanharstad <ethanharstad@gmail.com>
2013-05-10 17:39:22 -0500

    Added sounding controller skeleton

---
+++ controllers/soundings.js
@@ -0,0 +1,17 @@
+var Sounding = require('../models/sounding.js');
+
+exports.dateIndex = function(req, res) {
+
+};
+
+exports.timeIndex = function(req, res) {
+
+};
+
+exports.locationIndex = function(req, res) {
+
+};
+
+exports.show = function(req, res) {
+
+};
