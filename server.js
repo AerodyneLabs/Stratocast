f2918fdddb7c01a919ac1510cc67e72bec8148ad
ethanharstad <ethanharstad@gmail.com>
2013-05-11 01:41:49 -0500

    Initialize data store

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http');
 var mongoose = require('mongoose');
 var kue = require('kue');
 
+var ds = require('./lib/data-store');
+
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1/habweb');
 mongoose.connection.on('open', function() {
@@ -23,3 +25,5 @@ app.configure(function() {
 
 http.createServer(app).listen(80);
 console.log('Listening on port 80');
+
+ds.init();
