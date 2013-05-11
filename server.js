af8bc567e2bbd423572ed913447dcac51828e643
ethanharstad <ethanharstad@gmail.com>
2013-05-10 19:29:38 -0500

    Added kue to use as job queue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var mongoose = require('mongoose');
+var kue = require('kue');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1/habweb');
@@ -8,13 +9,17 @@ mongoose.connection.on('open', function() {
 	console.log('Connected to Mongoose');
 });
 
+// Create the job queue
+var jobs = kue.createQueue();
+kue.app.listen(8000);
 
 // Create the webserver
 app = express();
 
 app.configure(function() {
-	app.use(express.logger('default'));
+	app.use(express.logger('dev'));
 	app.use(express.static(__dirname + '/public'));
 });
 
-http.createServer(app).listen(8000);
+http.createServer(app).listen(80);
+console.log('Listening on port 80');
