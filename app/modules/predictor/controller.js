a6565e788edd2a1bf9e9315b28c4578e1f20639b
Ethan Harstad <ethanharstad@gmail.com>
2013-07-31 15:10:56 -0500

    Add flightTimeEditor view to prediction module.
    Create flightTimeEditor template and view. Add step 3 to
    ForwardPrediction application chain.

--- app/modules/predictor/controller.js
+++ app/modules/predictor/controller.js
@@ -20,6 +20,12 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
 				Mod.wizardLayout.body.show(Mod.leftSidebarLayout);
 				Mod.leftSidebarLayout.sidebar.show(new Mod.views.FlightParametersEditor());
 				break;
+			case 3:
+				Backbone.history.navigate('pred/forward/3');
+				App.content.show(Mod.wizardLayout);
+				Mod.wizardLayout.body.show(Mod.leftSidebarLayout);
+				Mod.leftSidebarLayout.sidebar.show(new Mod.views.FlightTimeEditor());
+				break;
 			default:
 				// Step One
 				Backbone.history.navigate('pred/forward/1');
---
+++ app/modules/predictor/views/flightTimeEditor.js
@@ -0,0 +1,21 @@
+App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
+
+	Mod.views.FlightTimeEditor = Marionette.ItemView.extend({
+		template: 'flightTimeEditor',
+		model: Mod.FlightModel,
+		tagName: 'form',
+		events: {
+			'click #next': 'next',
+			'click #prev': 'prev'
+		},
+
+		next: function() {
+			App.vent.trigger('ForwardPrediction:Display', 4);
+		},
+
+		prev: function() {
+			App.vent.trigger('ForwardPrediction:Display', 2)
+		}
+	});
+
+});
