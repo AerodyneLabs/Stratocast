9772ca7b5662fd0b79bbe44ebfa828be5ecce19e
Ethan Harstad <ethanharstad@gmail.com>
2013-08-02 19:50:18 -0500

    Added untested rendering to flightCalendarEditor.

--- app/modules/predictor/views/calendarDayView.js
+++ app/modules/predictor/views/calendarDayView.js
@@ -3,7 +3,47 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
   Mod.views.CalendarDayView = Marionette.ItemView.extend({
     template: 'calendarDayView',
     tagName: 'td',
-    className: 'media'
+    className: 'media',
+
+    serializeData: function() {
+      // Initialize the data JSON
+      var data = {};
+
+      // Ensure the model is defined
+      if(this.model) {
+        // Ensure the model has a timestamp
+        if(this.model.timestamp) {
+          // Create a js date object
+          var time = new Date(this.model.timestamp);
+          // Extract the day of month from timestamp
+          this.model.dayOfMonth = time.getDate();
+          // Extract the day of week from timestamp
+          this.model.dayOfWeek = time.getDay();
+        }
+
+        // Convert the model to JSON
+        data = this.model.toJSON();
+      }
+
+      // Return the data JSON
+      return data;
+    },
+
+    attributes: function() {
+      var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
+      // Initialize the attribute hash
+      var attr = {};
+      // Add the day attribute if it exists in the model
+      if(this.model.dayOfWeek) {
+        attr = {
+          'data-day': days[this.model.dayOfWeek]
+        };
+      }
+
+      // Return the attribute hash
+      return attr;
+    }
+
   });
 
 });
--- app/modules/predictor/views/flightCalendarEditor.js
+++ app/modules/predictor/views/flightCalendarEditor.js
@@ -5,16 +5,26 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
     tagName: 'div',
     id: 'calendar',
     itemView: Mod.views.CalendarDayView,
+    ui: {
+      row: 'tr:last',
+      tbody: 'tbody'
+    },
 
     appendHtml: function(compositeView, itemView, index) {
       // Default implementation
       // compositeView.$('tbody').append(itemView.el);
       if (index === 0) {
         // If first item, apply padding
+        for(var i = 0; i < itemView.model.dayOfWeek; i++) {
+          compositeView.ui.row.append('<td class="media unused"></td>');
+        }
       }
-      if (false) {
+      if (itemView.model.dayOfWeek === 0) {
         // If first day of week, create new row
+        compositeView.ui.tbody.append('<tr></tr>');
       }
+      // Append ui element
+      compositeView.ui.row.append(itemView.el);
     }
   });
 
