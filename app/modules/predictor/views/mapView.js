3d7d7803e787fcc12d7459ecd358258f730d31a9
ethanharstad <ethanharstad@gmail.com>
2013-08-18 01:17:25 -0500

    Add hybrid layer to map.

--- app/modules/predictor/views/mapView.js
+++ app/modules/predictor/views/mapView.js
@@ -12,16 +12,34 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
 		},
 
 		onShow: function() {
-			this.map = L.map('map').setView([42, -93], 13);
 			// TODO Map attribution
-			// TODO Sat layer
-			// TODO Hybrid layer
-			L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',{
+			var mapLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',{
 				attribution: 'Map data &copy;',
 				maxZoom: 18,
 				subdomains: '1234'
-			}).addTo(this.map);
+			});
+			var satLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg',{
+				attribution: 'Map data &copy;',
+				maxZoom: 18,
+				subdomains: '1234'
+			});
+			var hybLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/hyb/{z}/{x}/{y}.jpg',{
+				attribution: 'Map data &copy;',
+				maxZoom: 18,
+				subdomains: '1234'
+			});
+			var hybGroup = L.layerGroup([satLayer, hybLayer]);
+			var baseMaps = {
+				"Map": mapLayer,
+				"Hybrid": hybGroup
+			};
 			this.resize();
+			this.map = L.map('map', {
+				layers: [mapLayer],
+				center: new L.LatLng(42, -94),
+				zoom: 10
+			});
+			L.control.layers(baseMaps).addTo(this.map);
 		},
 
 		resize: function() {
