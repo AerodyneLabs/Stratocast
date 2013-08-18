681d43da02eda1a0601844a67dc61d0ab4860af8
ethanharstad <ethanharstad@gmail.com>
2013-08-18 16:00:09 -0500

    Use Marionette CDN.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ require.config({
     jquery: 'lib/jquery-1.9.1',
     bootstrap: ['//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min', 'lib/boostrap'],
     backbone: ['//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min', 'lib/backbone'],
-    marionette: 'lib/backbone.marionette',
+    marionette: ['//cdnjs.cloudflare.com/ajax/libs/backbone.marionette/1.0.4-bundled/backbone.marionette.min', 'lib/backbone.marionette'],
     handlebars: 'lib/handlebars',
     leaflet: '//cdn.leafletjs.com/leaflet-0.6.4/leaflet',
     modulehelper: 'modules/modulehelper'
