6f667d9820889d927f3ff8491d457934b5c61c3f
ethanharstad <ethanharstad@gmail.com>
2013-08-21 02:04:58 -0500

    Add geocoding to FlightLocationEditor
    Add search button to FlightLocationEditor template. Add keypress listener
    to js to support submit on enter. Add click listener to search for geocode.
    Recenter map on geocode success.

--- app/modules/predictor/views/flightLocationEditor.js
+++ app/modules/predictor/views/flightLocationEditor.js
@@ -5,11 +5,51 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
     model: Mod.FlightModel,
     tagName: 'form',
     events: {
-      'click #next': 'next'
+      'click #next': 'next',
+      'click #search': 'search',
+      'keypress': 'submitOnEnter'
+    },
+    ui: {
+      location: '#location',
+      icon: '#search-icon'
     },
 
     next: function() {
       App.vent.trigger('ForwardPrediction:Display', 2);
+    },
+
+    search: function() {
+      this.geocodeLocation(this.ui.location.val());
+    },
+
+    submitOnEnter: function(e) {
+      if(e.keyCode == 13) this.search();
+    },
+
+    geocodeLocation: function(query) {
+      var url = 'http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluub250229%2C25%3Do5-9u8ah0&location=';
+      var options = '&thumbMaps=false';
+      console.log(url + encodeURIComponent(query) + options);
+      this.ui.icon.addClass('icon-spin');
+      $.ajax(url + encodeURIComponent(query) + options, {
+        success: this.geocodeHandler,
+        error: this.geocodeError,
+        complete: this.geocodeComplete
+      });
+    },
+
+    geocodeHandler: function(data, status, jqXHR) {
+      var lat = data.results[0].locations[0].latLng.lat;
+      var lng = data.results[0].locations[0].latLng.lng;
+      App.vent.trigger('Map:Center', lat, lng);
+    },
+
+    geocodeError: function(jqXHR, status, error) {
+      console.log("Geocode Error: " + error);
+    },
+
+    geocodeComplete: function(jqXHR, status) {
+      $('#search-icon').removeClass('icon-spin');
     },
 
     onShow: function() {
