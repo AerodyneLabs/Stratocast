efe4227bdeac05fea0dac21354dbe6b746d49aad
ethanharstad <ethanharstad@gmail.com>
2013-08-20 23:04:50 -0500

    Update FlightLocationEditor on map click events
    Event populates location input box with clicked latitude and longitude.
    Add 'Map:Click' event handler on view 'onShow', remove on 'onClose'.

--- app/modules/predictor/views/flightLocationEditor.js
+++ app/modules/predictor/views/flightLocationEditor.js
@@ -10,6 +10,19 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
 
     next: function() {
       App.vent.trigger('ForwardPrediction:Display', 2);
+    },
+
+    onShow: function() {
+      App.vent.on('Map:Click', this.setLatLon);
+    },
+
+    onClose: function() {
+      App.vent.off('Map:Click', this.setLatLon);
+    },
+
+    setLatLon: function(e) {
+      var str = Math.round(e.latlng.lat*100)/100 + ', ' + Math.round(e.latlng.lng*100)/100;
+      $('#location').val(str);
     }
   });
 
