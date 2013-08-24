b307db9cce40792cb3a03a7c640707df72e702a4
ethanharstad <ethanharstad@gmail.com>
2013-08-23 20:51:57 -0500

    Add fields to AtmosphericObservation
    Add timestamp, latitude and longitude fields. Rewrite constructor to
    use input object instead of ordered parameter list. Rewrite test specs
    to use new constructor format.
    Closes #19

--- spec/atmosphere/atmosphericObservation.spec.js
+++ spec/atmosphere/atmosphericObservation.spec.js
@@ -3,7 +3,13 @@ var Observation = require('../../lib/atmosphere').AtmosphericObservation;
 describe('AtmosphericObservation object', function() {
 
 	it('holds the observed values', function() {
-		var obs = new Observation(1000, 2000, 273.15, 1, 2);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: 1,
+			windV: 2
+		});
 		expect(obs.altitude).toEqual(1000);
 		expect(obs.pressure).toEqual(2000);
 		expect(obs.temperature).toEqual(273.15);
@@ -12,27 +18,57 @@ describe('AtmosphericObservation object', function() {
 	});
 
 	it('computes wind speed', function() {
-		var obs = new Observation(0, 0, 0, 3, 4);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: 3,
+			windV: 4
+		});
 		expect(obs.getWindSpeed()).toEqual(5);
 	});
 
 	it('computes wind direction in NE quadrant', function() {
-		var obs = new Observation(0, 0, 0, 1, 1);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: 1,
+			windV: 1
+		});
 		expect(obs.getWindDirection()).toEqual(3 * Math.PI / 4);
 	});
 
 	it('computes wind direction in SE quadrant', function() {
-		var obs = new Observation(0, 0, 0, 1, -1);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: 1,
+			windV: -1
+		});
 		expect(obs.getWindDirection()).toEqual(Math.PI / 4);
 	});
 
 	it('computes wind direction in SW quadrant', function() {
-		var obs = new Observation(0, 0, 0, -1, -1);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: -1,
+			windV: -1
+		});
 		expect(obs.getWindDirection()).toEqual(-1 * Math.PI / 4);
 	});
 
 	it('computes wind direction in NW quadrant', function() {
-		var obs = new Observation(0, 0, 0, -1, 1);
+		var obs = new Observation({
+			altitude: 1000,
+			pressure: 2000,
+			temperature: 273.15,
+			windU: -1,
+			windV: 1
+		});
 		expect(obs.getWindDirection()).toEqual(-3 * Math.PI / 4);
 	});
 
