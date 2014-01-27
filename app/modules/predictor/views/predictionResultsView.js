c75bebc1367e828bc036df1de51d826998939583
Ethan Harstad <ethanharstad@gmail.com>
2014-01-26 22:27:07 -0600

    Populate remaining fields in predictionResultsView
    Add speed and bearing template helper functions.
    Compute bearing and total distance in prediction runner.

--- app/modules/predictor/views/predictionResultsView.js
+++ app/modules/predictor/views/predictionResultsView.js
@@ -41,6 +41,21 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
 
         return value + ' ' + unit;
       },
+      convertSpeed: function(data) {
+        var value = data || 0.0;
+        var unit = 'm/s';
+
+        if(App.unitSystem === 'english') {
+          unit = "fpm";
+          value /= 196.9;
+          value = Math.round(value);
+        } else {
+          // default is already in metric
+          value = Math.round(value * 1000) / 1000;
+        }
+
+        return value + ' ' + unit;
+      },
       absoluteTime: function(data) {
         var time = new Date(data);
         var ret = (time.getMonth() + 1) + "/" + time.getDate() + "/" + time.getFullYear();
@@ -58,6 +73,10 @@ App.module("Predictor", function(Mod, App, Backbone, Marionette, $, _) {
         var lat = Math.round(data[1] * 1000) / 1000;
         var lon = Math.round(data[0] * 1000) / 1000;
         return lat + ', ' + lon;
+      },
+      angle: function(data) {
+        var temp = (data + 360) % 360;
+        return Math.round(temp * 100) / 100;
       }
     },
 
